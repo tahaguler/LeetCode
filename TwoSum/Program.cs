beda98ecc5184f20e11b4e82161246ec0cd245eb
Taha <tahaguler95@hotmail.com>
2021-04-08 19:26:59 -0400

    TwoSum copied solution

---
+++ TwoSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {2, 7, 11, 15};
+            int target = 9;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (dict.ContainsKey(complement))
+                {
+                    Console.WriteLine('[' + dict.GetValueOrDefault(complement).ToString() + ", " + i + ']');
+                    return;
+                }
+
+                dict.Add(nums[i], i);
+            }
+
+            throw new Exception("No two sum solution");
+        }
+    }
+}
